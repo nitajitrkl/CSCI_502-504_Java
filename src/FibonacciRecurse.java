33c39d039514ffe071782f9af2656d4ecf6fc32b
Ajit Behera <41351154+nitajitrkl@users.noreply.github.com>
2023-11-16 13:55:03 -0500

    Project clarified

--- src/FibonacciRecurse.java
+++ src/FibonacciRecurse.java
@@ -1,12 +1,8 @@
 public class FibonacciRecurse {
-
     public void printFibonacci(int start, int end, int n) {
         if (n > 0) {
             System.out.print(" " + start);
-            int next = start + end;
-            start = end;
-            end = next;
-            printFibonacci(start, end, n - 1);
+            printFibonacci(end, start + end, n - 1);
         }
     }
 }
