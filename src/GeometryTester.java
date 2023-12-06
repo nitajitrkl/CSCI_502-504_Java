4a2547691841a3a38b287ffb9973bbe154011aae
Ajit Behera <41351154+nitajitrkl@users.noreply.github.com>
2023-12-06 16:43:29 -0500

    HW6 Q3 Solved

---
+++ src/GeometryTester.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class GeometryTester{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter the radius: ");
+        double r = in.nextDouble();
+
+        System.out.println("Please enter the height: ");
+        double h = in.nextDouble();
+
+        System.out.println("The volume of the sphere is: "
+                + Geometry.sphereVolume(r));
+        System.out.println("The surface area of the sphere is: "
+                + Geometry.sphereSurface(r));
+        System.out.println("The volume of the cylinder is: "
+                + Geometry.cylinderVolume(r, h));
+        System.out.println("The surface area of the cylinder is: "
+                + Geometry.cylinderSurface(r, h));
+        System.out.println("The volume of the cone is: "
+                + Geometry.coneVolume(r, h));
+        System.out.println("The surface area of the cone is: "
+                + Geometry.coneSurface(r, h));
+
+    }
+}
