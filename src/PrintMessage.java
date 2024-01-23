0923216833c92b0056848b415b4197389e03f4c4
Ajit Behera <41351154+nitajitrkl@users.noreply.github.com>
2024-01-22 19:00:36 -0500

    dadad af;

---
+++ src/PrintMessage.java
@@ -0,0 +1,12 @@
+public class PrintMessage {
+    public static void printMessagee (String message, int n){
+        for (int i = 0; i< n; i++){
+            System.out.println(message);
+        }
+    }
+    public static void PrintMessagee (){
+        System.out.println("Happy Fall 2019");
+        System.out.println("Happy Fall 2019");
+        System.out.println("Happy Fall 2019");
+    }
+}
---
+++ src/PrintMessageTester.java
@@ -0,0 +1,6 @@
+public class PrintMessageTester {
+    public static void main(String[] args) {
+        PrintMessage.PrintMessagee();
+        PrintMessage.printMessagee("Happy", 4);
+    }
+}
