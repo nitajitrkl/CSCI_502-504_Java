32335e07d564e6df8d76bd3b589778d56da8067c
Ajit Behera <41351154+nitajitrkl@users.noreply.github.com>
2024-02-11 20:47:43 -0500

    assignment 2

--- src/DerivedTester.java
+++ src/DerivedTester.java
@@ -3,7 +3,7 @@ import p1.Base;
 public class DerivedTester extends Base {
     public static void main(String[] args) {
         DerivedTester sample = new DerivedTester();
-        sample.accessProtectedMethod();
+        sample.accessProtectedMethod(); 
     }
     public void accessProtectedMethod() {
         show();
--- src/p1/Base.java
+++ src/p1/Base.java
@@ -3,5 +3,6 @@ package p1;
 public class Base {
     protected void show() {
         System.out.println("Message!!!");
+        System.out.println("Hello World!!!");
     }
 }
