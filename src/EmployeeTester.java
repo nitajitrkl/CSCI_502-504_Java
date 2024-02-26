f2dbc37ef193769c4916be72d925a50b256a1559
Ajit Behera <nitajitrkl@gmail.com>
2024-02-26 17:26:49 -0500

    done

--- src/EmployeeTester.java
+++ src/EmployeeTester.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class EmployeeTester {
     public static void main(String[] args) {
         ArrayList<Employee> employess = new ArrayList<>();
-        employess.add(new Manager1("Ajit", 3000, "Recusrion"));
+        employess.add(new Manager1("Ajit", 3000, "Recursion"));
         employess.add(new Developer("Amit", 4000, "Python"));
         for (Employee employee : employess) {
             employee.displayDetails();
