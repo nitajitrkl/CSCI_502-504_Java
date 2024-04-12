1602a8a68fbe9e911be293e1bbabb5c947ee38c1
Ajit Behera <nitajitrkl@gmail.com>
2024-04-12 18:00:25 -0400

    mab assignment completed for CSCI 651 12th April

---
+++ src/Merge_sort.java
@@ -0,0 +1,75 @@
+import java.util.Random;
+
+//CSCI651
+//LAB: Analysis of Sorting Algorithms
+public class Merge_sort {
+    public void merge(int[] arr, int l, int m, int r) // Merge two subarrays L and M into arr
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int[] L = new int[n1];
+        int R[] = new int[n2];
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+// Maintain current index of sub-arrays and main array
+        int i = 0, j = 0;
+        int k = l;
+// Until we reach either end of either L or M, pick larger among
+// elements L and M and place them in the correct position at
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+// When we run out of elements in either L or M,
+// pick up the remaining elements and put in A[p..r]
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    public void sort(int[] arr, int l, int r)
+    {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+    public static void display(int[] a) {
+        for(int i:a){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+        Random rand = new Random();
+        int[] arr = new int[100000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(100000);
+        }
+        Merge_sort ms = new Merge_sort();
+        ms.sort(arr, 0, arr.length - 1);
+        display(arr);
+        long endTime   = System.nanoTime();
+        long totalTime = endTime - startTime;
+        System.out.println("Total time: " + totalTime);
+    }
+}
---
+++ src/Selection_Sort.java
@@ -0,0 +1,44 @@
+//CSCI651 
+//LAB: Analysis of Sorting Algorithms
+
+import java.util.Random;
+
+public class Selection_Sort {
+    public void selectionSort(int[] a) {
+        for(int i=0; i<a.length-1; i++) {
+            int min_index=i;
+            for(int j = i+1; j<a.length; j++) {
+                if(a[j] < a[min_index]) {
+                    min_index = j;
+                }
+            }
+// put the minimum element in the array
+            int temp = a[min_index];
+            a[min_index] = a[i];
+            a[i] = temp;
+        }
+    }
+    public void display(int[] a) {
+        for(int i:a){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+        Random rand = new Random();
+        int[] a = new int[100000];
+        for(int i=0; i<a.length; i++) {
+            a[i] = rand.nextInt(100000);
+        }
+        Selection_Sort sort = new Selection_Sort();
+        sort.selectionSort(a);
+        sort.display(a);
+        long endTime   = System.nanoTime();
+        long totalTime = endTime - startTime;
+        System.out.println("Total time: " + totalTime);
+    }
+}
+
+
