5b5d793c6ca8e050ae89e1cd25e4024c957bcdc0
Ajit Behera <nitajitrkl@gmail.com>
2024-05-06 22:05:20 -0400

    class assignment 22nd April 2024 CSCI 504

---
+++ src/SumRecursion.java
@@ -0,0 +1,12 @@
+public class SumRecursion {
+    public static int recursion(int n){
+        if(n == 0){
+            return 1;
+        }
+        return n + recursion(n-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(SumRecursion.recursion(5));
+    }
+}
